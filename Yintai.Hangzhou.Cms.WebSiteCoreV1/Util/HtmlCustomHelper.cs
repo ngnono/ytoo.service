db5e4926cfb578c41d3d55e8238e732385f57da8
victor <gyyshuai@gmail.com>
2013-02-07 21:27:32 +0800

    add some files

---
+++ Yintai.Hangzhou.Cms.WebSiteCoreV1/Util/HtmlCustomHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Yintai.Hangzhou.Cms.WebSiteCoreV1.Util
+{
+    public static class HtmlCustomHelper
+    {
+        public static string ClientId<TModel>(this HtmlHelper helper, Expression<Func<TModel, TModel>> ex)
+        {
+            return helper.ViewData.TemplateInfo.GetFullHtmlFieldId(ExpressionHelper.GetExpressionText(ex));
+        }
+    }
+}
