77b23e1a54ffef78db1df6ac6f604a1932087d04
ngnono <ngnono@126.com>
2013-03-21 10:21:40 +0800

    g OK

---
+++ Yintai.Hangzhou.Model/Filters/PromotionFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yintai.Architecture.Common.Models;
+using Yintai.Hangzhou.Model.Enums;
+
+namespace Yintai.Hangzhou.Model.Filters
+{
+    public class PromotionFilter
+    {
+        public Timestamp Timestamp { get; set; }
+
+        public PromotionFilterMode? FilterMode { get; set; }
+
+        public DataStatus? DataStatus { get; set; }
+
+        public bool? HasBanner { get; set; }
+
+        public bool? HasProduct { get; set; }
+    }
+}
