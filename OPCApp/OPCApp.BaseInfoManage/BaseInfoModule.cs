d47b25c06ed46f3fab2f0524ebe1e171ea767dfd
shimerlj <lj841205>
2014-03-09 23:40:28 +0800

    1

--- OPCApp/OPCApp.BaseInfoManage/BaseInfoModule.cs
+++ OPCApp/OPCApp.BaseInfoManage/BaseInfoModule.cs
@@ -12,7 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 namespace OPCApp.BaseInfoManage
 {
-    //test push
+    //test push   324324
     [ModuleExport(typeof(BaseInfoModule))]
     public class BaseInfoModule : IModule
     {
