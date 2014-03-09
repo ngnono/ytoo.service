38f9bb1085bc0ecac264fccf7cff6284bf5ed813
shimerlj <lj841205>
2014-03-09 23:31:57 +0800

    333

--- OPCApp/OPCApp.BaseInfoManage/BaseInfoModule.cs
+++ OPCApp/OPCApp.BaseInfoManage/BaseInfoModule.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 namespace OPCApp.BaseInfoManage
 {
+    //
     [ModuleExport(typeof(BaseInfoModule))]
     public class BaseInfoModule : IModule
     {
