206b4d4f26b7962c84d4eff11d624b58a2c02323
liuyh208 <liuyh208@sohu.com>
2014-03-09 22:25:32 +0800

    test push

--- OPCApp/OPCApp.Order/Views/OrderGoodsInfoView.xaml.cs
+++ OPCApp/OPCApp.Order/Views/OrderGoodsInfoView.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : OPCApp.Order
+// Author           : Liuyh
+// Created          : 03-09-2014 22:15:01
+//
+// Last Modified By : Liuyh
+// Last Modified On : 02-21-2014 22:50:48
+// ***********************************************************************
+// <copyright file="OrderGoodsInfoView.xaml.cs" company="liuyh">
+//     Copyright (c) liuyh. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -15,6 +28,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OPCApp.Order.ViewModels;
 
+/// <summary>
+/// The Views namespace.
+/// </summary>
 namespace OPCApp.Order.Views
 {
     /// <summary>
@@ -24,11 +40,18 @@ namespace OPCApp.Order.Views
     //[PartCreationPolicy(CreationPolicy.NonShared)]
     public partial class OrderGoodsInfoView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderGoodsInfoView"/> class.
+        /// </summary>
         public OrderGoodsInfoView()
         {
             InitializeComponent();
         }
         //[Import]
+        /// <summary>
+        /// Sets the view model.
+        /// </summary>
+        /// <value>The view model.</value>
         public OrderGoodsInfoViewModel ViewModel
         {
             set { this.DataContext = value; }
