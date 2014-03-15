83faca965f7453a13f5e4d8318a94770921a5be9
liuyh208 <liuyh208@sohu.com>
2014-03-15 21:06:28 +0800

    rename

--- OPCApp/OPCApp.AuthManage/ViewModels/Role/RoleListViewModel.cs
+++ OPCApp/OPCApp.AuthManage/ViewModels/Role/RoleListViewModel.cs
@@ -1,25 +1,46 @@
-﻿using System.ComponentModel.Composition;
-using OPCApp.AuthManage.Views;
+﻿// ***********************************************************************
+// Assembly         : OPCApp.AuthManage
+// Author           : Liuyh
+// Created          : 03-15-2014 14:47:14
+//
+// Last Modified By : Liuyh
+// Last Modified On : 03-15-2014 21:03:27
+// ***********************************************************************
+// <copyright file="RoleListViewModel.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 using OPCApp.DataService.Interface;
 using OPCApp.Domain;
-using OPCApp.DataService.Impl;
 using OPCApp.Infrastructure;
 using OPCApp.Infrastructure.DataService;
 using OPCApp.Infrastructure.Mvvm;
+using System.ComponentModel.Composition;
 namespace OPCApp.AuthManage.ViewModels
 {
 
+    /// <summary>
+    /// Class RoleListViewModel.
+    /// </summary>
     [Export("RoleListViewModel", typeof(IViewModel))]
     [PartCreationPolicy(CreationPolicy.NonShared)]
-    public class RoleListViewModel :BaseCollectionViewModel<Role>
+    public class RoleListViewModel :BaseListViewModel<Role>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoleListViewModel"/> class.
+        /// </summary>
         public RoleListViewModel()
             : base("RoleListWindow")
         {
             this.EditViewModeKey = "RoleViewModel";
             this.AddViewModeKey = "RoleViewModel";
         }
-       
+
+        /// <summary>
+        /// Gets the data service.
+        /// </summary>
+        /// <returns>IBaseDataService{Role}.</returns>
         protected override IBaseDataService<Role> GetDataService()
         {
             return AppEx.Container.GetInstance<IRoleDataService>();
