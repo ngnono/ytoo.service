43648eb74d7d850cc982a879eab3dd2875e185d0
hanyuxing5588 <707297069@qq.com>
2014-03-15 22:57:31 +0800

    auth inherit do

--- OPCApp/OPCApp.AuthManage/Views/UserAddWindow.xaml.cs
+++ OPCApp/OPCApp.AuthManage/Views/UserAddWindow.xaml.cs
@@ -1,44 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using MahApps.Metro;
 using MahApps.Metro.Controls;
 using OPCApp.AuthManage.ViewModels;
+using System.ComponentModel.Composition;
+using OPCApp.Infrastructure.Mvvm.View;
 
 namespace OPCApp.AuthManage.Views
 {
-    /// <summary>
-    /// UserAddWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class UserAddWindow : MetroWindow
+   [Export("UserView", typeof(IBaseView))]
+   [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class UserAddWindow : MetroWindow, IBaseView
     {
-        public UserAddWindowViewModel UserAddWin = new UserAddWindowViewModel();
+        
         public UserAddWindow()
         {
             InitializeComponent();
-            UserAddWin.SubmitCommand = new Microsoft.Practices.Prism.Commands.DelegateCommand(new Action(Submit));
-            UserAddWin.ResetCommand = new Microsoft.Practices.Prism.Commands.DelegateCommand(new Action(Cancel));
-            this.DataContext = UserAddWin;
 
-        }
-        public void Submit() 
-        {
-            this.DialogResult = true;
-            this.Close();
         }
         public void Cancel() 
         {
             this.DialogResult = false;
+            this.Close();
+        }
+
+        public void CloseView()
+        {
+            this.DialogResult = true;
             this.Close();
         }
     }
