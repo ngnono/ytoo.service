d5ed742d777560d8c9917f26e898c0e234322800
hanyuxing5588 <707297069@qq.com>
2014-03-17 22:31:29 +0800

    1

---
+++ OPCApp/OPCApp.AuthManage/Views/UsersWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MahApps.Metro.Controls;
+using OPCApp.AuthManage.ViewModels;
+using System.ComponentModel.Composition;
+using OPCApp.Infrastructure.Mvvm.View;
+
+namespace OPCApp.AuthManage.Views
+{
+   [Export("UserView", typeof(IBaseView))]
+   [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class UserAddWindow : MetroWindow, IBaseView
+    {
+        
+        public UserAddWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void Cancel() 
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        public void CloseView()
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
