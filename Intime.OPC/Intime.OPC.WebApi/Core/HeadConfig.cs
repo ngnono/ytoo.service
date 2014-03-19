da893f59fb0ab4a22e7e0d4f9498f7c588ec4199
zxy98 <zxy98@126.com>
2014-03-19 16:57:54 +0800

    zxy

---
+++ Intime.OPC/Intime.OPC.WebApi/Core/HeadConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Intime.OPC.WebApi.Core
+{
+    public static class HeadConfig
+    {
+        public const string Sign = "X-Sign";
+        public const string Toekn = "X-Token";
+        public const string Consumerkey = "X-ConsumerKey";
+    }
+}
