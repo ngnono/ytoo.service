68b4052699644799c7a0b7c73a1172cec287c6aa
zxy98 <zxy98@126.com>
2014-03-19 16:55:11 +0800

    zxy

---
+++ Intime.OPC/Intime.OPC.Repository/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Intime.OPC.Domain.Models;
+using System.Collections.Generic;
+
+namespace Intime.OPC.Repository
+{
+    public interface IRoleRepository
+    {
+        bool Create(OPC_AuthRole role);
+        bool Update(OPC_AuthRole role);
+        bool Delete(int roleId);
+        IList<OPC_AuthRole> Select();
+        bool IsStop(int roleId, bool bValid);
+    }
+}
