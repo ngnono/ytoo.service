5a352a4a263ec77add5a55428190376738486f1a
hanyuxing5588 <707297069@qq.com>
2014-03-19 21:13:36 +0800

    auth i

---
+++ OPCApp/OPCAPP.DataService/Interface/Auth/IRole2UserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using OPCApp.Domain.Models;
+using OPCApp.Infrastructure.DataService;
+
+namespace OPCApp.DataService.Interface
+{
+    public interface IRole2UserService
+    {
+        ResultMsg SetUserByRole(int roleId,List<int> listUserId);
+    }
+}
