78f584c56ed03f76a73f6cb2d27d08578d8a08f7
hanyuxing5588 <707297069@qq.com>
2014-03-19 20:16:25 +0800

    1

--- OPCApp/OPCAPP.Domain/Models/OPC_AuthUser.cs
+++ OPCApp/OPCAPP.Domain/Models/OPC_AuthUser.cs
@@ -1,13 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OPCApp.Domain.Models
 {
     public class OPC_AuthUser
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required(ErrorMessage = "用户名不能为空！")]
         public int? SectionId { get; set; }
+        [Required(ErrorMessage = "登陆名不能为空！")]
         public string LogonName { get; set; }
+        [Required(ErrorMessage = "密码不能为空！")]
         public string Password { get; set; }
         public string Phone { get; set; }
         public bool? IsValid { get; set; }
--- OPCApp/OPCAPP.Domain/Role.cs
+++ OPCApp/OPCAPP.Domain/Role.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace  OPCApp.Domain
 {
-    public class Role
-    {
-        public Role()
-        { }
-        public string RoleName { get; set; }
-        public string Description { get; set; }
-        public int Id { get; set; }
-    }
+    //public class Role
+    //{
+    //    public Role()
+    //    { }
+    //    public string RoleName { get; set; }
+    //    public string Description { get; set; }
+    //    public int Id { get; set; }
+    //}
 
 }
