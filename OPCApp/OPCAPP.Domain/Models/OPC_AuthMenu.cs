49f6d29af7c926dcea67162d6d2475db08b6a44f
hanyuxing5588 <707297069@qq.com>
2014-03-20 01:26:14 +0800

    domain

--- OPCApp/OPCAPP.Domain/Models/OPC_AuthMenu.cs
+++ OPCApp/OPCAPP.Domain/Models/OPC_AuthMenu.cs
@@ -5,6 +5,7 @@ namespace OPCApp.Domain.Models
     public class OPC_AuthMenu
     {
         public int Id { get; set; }
+        public bool IsSelected { get; set; }
         public string MenuName { get; set; }
         public DateTime CreateDate { get; set; }
         public int CreateUserId { get; set; }
---
+++ OPCApp/OPCAPP.Domain/RoleMenuDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Domain.Dto
+{
+    public  class RoleAuthDto
+    {
+        public int  RoleID { get; set; }
+
+        public int UserID { get; set; }
+
+        /*菜单IDS 或者 用户IDS*/
+        public IList<int> IDList { get; set; }
+    }
+}
