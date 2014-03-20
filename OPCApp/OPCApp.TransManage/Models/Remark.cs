5a9bebef80b97c363badf1a887e2f599f6165a29
zxy98 <zxy98@126.com>
2014-03-20 11:30:20 +0800

    zxy 32

---
+++ OPCApp/OPCApp.TransManage/Models/Remark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCApp.TransManage.Models
+{
+    public class Remark
+    {
+        public string OrderID { get; set; }
+        public string InvoiceID { get; set; }
+        public string SalesID { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ OPCApp/OPCApp.TransManage/Views/RemarkWin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Practices.Prism.Commands;
+using OPCApp.TransManage.IService;
+using OPCApp.TransManage.ViewModels;
+
+namespace OPCApp.TransManage.Views
+{
+    /// <summary>
+    /// RemarkWin.xaml 的交互逻辑
+    /// 封装的录入备注的接口
+    /// </summary>
+    public partial class RemarkWin : IRemark
+    {
+        public RemarkViewModel sv = new RemarkViewModel();
+        public RemarkWin()
+        {
+            InitializeComponent();
+            sv.CommandSave = new DelegateCommand(new Action(this.CommandSaveExecute));
+            sv.CommandBack = new DelegateCommand(new Action(this.CommandBackExecute));
+            this.DataContext = sv;
+        }
+
+        public void ShowRemarkWin()
+        {
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            if (this.ShowDialog() == true)
+            {
+                this.sv.SaveRemark();
+                
+            }
+        }
+        public void CommandBackExecute()
+        {
+            DialogResult = false;
+            this.Close();
+        }
+
+        private void CommandSaveExecute()
+        {
+            DialogResult = true;
+            this.Close();
+        }
+    }
+}
