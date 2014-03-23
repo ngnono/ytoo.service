02e590f0365cc5d412fb00a55e1f37b1a53e93be
hanyuxing5588 <707297069@qq.com>
2014-03-23 17:45:47 +0800

    1

--- OPCApp/OPCAPP.DataService/Impl/Trans/RemarkOrderService.cs
+++ OPCApp/OPCAPP.DataService/Impl/Trans/RemarkOrderService.cs
@@ -15,20 +15,20 @@ using OPCApp.Infrastructure.DataService;
 
 namespace OPCApp.DataService.Impl.Trans
 {
-    [Export(typeof(IRemarkService))]
-    public class RemarkOrderService : IRemarkService
-    {
-        /*请李建同学从新改下*/
-        public bool WriteRemark(OPC_SaleComment saleComment)
-        {
-            bool bFalg = RestClient.Put("order/writeremark", saleComment);
-            return bFalg;
-        }
+    //[Export(typeof(IRemarkService))]
+    //public class RemarkOrderService : IRemarkService
+    //{
+    //    /*请李建同学从新改下*/
+    //    public bool WriteRemark(OPC_SaleComment saleComment)
+    //    {
+    //        bool bFalg = RestClient.Put("order/writeremark", saleComment);
+    //        return bFalg;
+    //    }
 
-        PageResult<OPC_SaleComment> IRemarkService.SelectRemark(string selectRemarkIds)
-        {
-            var lst = RestClient.Get<OPC_SaleComment>("order/selectremark", selectRemarkIds);
-            return new PageResult<OPC_SaleComment>(lst, lst.Count);
-        }
-    }
+    //    PageResult<OPC_SaleComment> IRemarkService.SelectRemark(string selectRemarkIds)
+    //    {
+    //        var lst = RestClient.Get<OPC_SaleComment>("order/selectremark", selectRemarkIds);
+    //        return new PageResult<OPC_SaleComment>(lst, lst.Count);
+    //    }
+    //}
 }
