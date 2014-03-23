813b5391dbcdd8074f016a561bfac8e164db16e3
zxy98 <zxy98@126.com>
2014-03-23 11:55:00 +0800

    zxy

---
+++ OPCApp/OPCApp.ReturnGoodsManage/View/PackageInfoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OPCApp.ReturnGoodsManage.View
+{
+    /// <summary>
+    /// PackageInfoView.xaml 的交互逻辑
+    /// </summary>
+    public partial class PackageInfoView : UserControl
+    {
+        public PackageInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
