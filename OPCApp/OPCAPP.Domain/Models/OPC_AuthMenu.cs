b76a5a466c8d6b369535e4cd5398657cbe5dc51f
hanyuxing5588 <707297069@qq.com>
2014-03-23 12:33:16 +0800

    load menu

--- OPCApp/OPCAPP.Domain/Models/OPC_AuthMenu.cs
+++ OPCApp/OPCAPP.Domain/Models/OPC_AuthMenu.cs
@@ -5,11 +5,10 @@ namespace OPCApp.Domain.Models
     public class OPC_AuthMenu
     {
         public int Id { get; set; }
-        public bool IsSelected { get; set; }
         public string MenuName { get; set; }
-        public DateTime CreateDate { get; set; }
-        public int CreateUserId { get; set; }
-        public DateTime UpdateDate { get; set; }
-        public int UpdateUserId { get; set; }
+        public int PraentMenuId { get; set; }
+        public int Sort { get; set; }
+        public string Url { get; set; }
+      
     }
 }
