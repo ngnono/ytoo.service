d8a48a2109f750a7543798e017cf2c2380596bc7
hanyuxing5588 <707297069@qq.com>
2014-03-23 11:25:52 +0800

    add menu reapository

---
+++ Intime.OPC/Intime.OPC.Repository/IMenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Intime.OPC.Domain.Models;
+using System.Collections.Generic;
+
+namespace Intime.OPC.Repository
+{
+    public interface IMenuRepository:IRespository<OPC_AuthMenu>
+    {
+      
+    }
+}
