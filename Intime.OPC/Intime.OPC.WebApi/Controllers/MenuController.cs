63647fa96cd905cc8248f19f4c09177a16f051d7
hanyuxing5588 <707297069@qq.com>
2014-03-23 11:41:54 +0800

    add menu

---
+++ Intime.OPC/Intime.OPC.WebApi/Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+using Intime.OPC.Domain.Models;
+using Intime.OPC.Service;
+using Intime.OPC.WebApi.Core.Security;
+using Intime.OPC.WebApi.Models;
+
+namespace Intime.OPC.WebApi.Controllers
+{
+    /// <summary>
+    ///     账户相关接口
+    /// </summary>
+    public class MenuController : ApiController
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+     
+        [HttpGet]
+        public IHttpActionResult LoadMenu()
+        {
+
+            //TODO:check params
+            return Ok(_menuService.Select());
+        }
+     
+    }
+}
