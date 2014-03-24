06e000337a030a069f7d01ba23864a59392f3f96
hanyuxing5588 <707297069@qq.com>
2014-03-25 00:32:05 +0800

    1

---
+++ OPCApp/OPCApp.AuthManage/Views/OrgManage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace OPCApp.BaseInfoManage.Views
+{
+    /// <summary>
+    /// OrgManage.xaml 的交互逻辑
+    /// </summary>
+    public partial class OrgManage 
+    {
+        public OrgManage()
+        {
+            //InitializeComponent();
+
+            //this.DataContext = new OrgManageViewModel();
+        }
+    }
+}
