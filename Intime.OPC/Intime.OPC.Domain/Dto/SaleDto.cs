9459e46129b703be6b9b7a3217405867e250dc0a
hanyuxing5588 <707297069@qq.com>
2014-03-26 00:04:04 +0800

    Merge branch 'vopc' of https://github.com/victorgong/ytoo.service into vopc
    Conflicts:
    	Intime.OPC/Intime.OPC.Repository/ISaleRepository.cs
    	Intime.OPC/Intime.OPC.Repository/Support/SaleRepository.cs
    	Intime.OPC/Intime.OPC.Repository/Support/TransRepository.cs
    	Intime.OPC/Intime.OPC.Service/ISaleService.cs
    	Intime.OPC/Intime.OPC.Service/Support/SaleService.cs
    	Intime.OPC/Intime.OPC.WebApi/Core/MessageHandlers/AccessToken/UserIdFilterAttribute.cs
    	OPCApp/OPCAPP.DataService/Impl/Auth/MenuDataService.cs
    	OPCApp/OPCApp.TransManage/OPCApp.TransManage.csproj

---
+++ Intime.OPC/Intime.OPC.Domain/Dto/SaleDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intime.OPC.Domain.Models;
+
+namespace Intime.OPC.Domain.Dto
+{
+    public class SaleDto 
+    {
+        public int Id { get; set; }
+        public string OrderNo { get; set; }
+        public string SaleOrderNo { get; set; }
+        public int SalesType { get; set; }
+        public Nullable<int> ShipViaId { get; set; }
+        public int Status { get; set; }
+        public Nullable<int> ShippingCode { get; set; }
+        public decimal ShippingFee { get; set; }
+        public Nullable<int> ShippingStatus { get; set; }
+        public string ShippingRemark { get; set; }
+        public System.DateTime SellDate { get; set; }
+        public Nullable<bool> IfTrans { get; set; }
+        public Nullable<int> TransStatus { get; set; }
+        public decimal SalesAmount { get; set; }
+        public Nullable<int> SalesCount { get; set; }
+        public Nullable<int> CashStatus { get; set; }
+        public string CashNum { get; set; }
+        public Nullable<System.DateTime> CashDate { get; set; }
+        public Nullable<int> SectionId { get; set; }
+        public Nullable<int> PrintTimes { get; set; }
+        public string Remark { get; set; }
+        public Nullable<System.DateTime> RemarkDate { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public int CreatedUser { get; set; }
+        public System.DateTime UpdatedDate { get; set; }
+        public int UpdatedUser { get; set; }
+        public string StatusName { get; set; }
+    }
+}
