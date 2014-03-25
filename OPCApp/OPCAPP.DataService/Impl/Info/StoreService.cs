35d23194d14eb75fc68ee964b5e490c7581d68e4
hanyuxing5588 <707297069@qq.com>
2014-03-25 23:53:09 +0800

    1

--- OPCApp/OPCAPP.DataService/Impl/Info/StoreService.cs
+++ OPCApp/OPCAPP.DataService/Impl/Info/StoreService.cs
@@ -1,13 +1,62 @@
-﻿using Intime.OPC.ApiClient;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using OPCApp.DataService.Common;
 using OPCApp.DataService.Interface;
-using OPCApp.Domain;
-using OPCApp.Domain.Models;
 using OPCApp.Infrastructure;
 using OPCApp.Infrastructure.DataService;
-using System;
+using OPCApp.Domain.Models;
 
 namespace OPCApp.DataService.Impl.Info
 {
- 
+    [Export(typeof(IStoreDataService))]
+    public class RoleDataService : IStoreDataService
+    {
+        private string url = "Role/AddRole";
+        public ResultMsg Add(OPC_AuthRole model)
+        {
+            var result = RestClient.Post<OPC_AuthRole>("Role/AddRole", model);
+            if (result)
+            {
+                return ResultMsg.Success();
+            }
+            return ResultMsg.Failure("添加失败！");
+        }
+
+        public ResultMsg Edit(OPC_AuthRole model)
+        {
+            var result = RestClient.Post<OPC_AuthRole>("Role/UpdateRole", model);
+            if (result)
+            {
+                return ResultMsg.Success();
+            }
+            return ResultMsg.Failure("更新失败！");
+        }
+
+        public ResultMsg Delete(OPC_AuthRole model)
+        {
+            var result = RestClient.Delete<OPC_AuthRole>("Role/DeleteRole/" + model.Id);
+            if (result)
+            {
+                return ResultMsg.Success();
+            }
+            return ResultMsg.Failure("删除失败！");
+        }
+
+        public PageResult<OPC_AuthRole> Search(IFilter filter)
+        {
+
+            var result = RestClient.Get<OPC_AuthRole>("Role/SelectRole", null);
+            return new PageResult<OPC_AuthRole>(result, result.Count);
+        }
+
+
+        public PageResult<OPC_AuthRole> Search(IDictionary<string, object> iDicFilter)
+        {
+            var result = RestClient.Get<OPC_AuthRole>("Role/SelectRole", null);
+            return new PageResult<OPC_AuthRole>(result, result.Count);
+        }
+
+
+
+    }
 }
--- OPCApp/OPCAPP.DataService/Interface/Info/IStoreService.cs
+++
@@ -1,9 +0,0 @@
-﻿using OPCApp.Domain.Models;
-namespace OPCApp.DataService.Interface
-{
-    public interface IStoreService : OPCApp.Infrastructure.DataService.IBaseDataService<OPC_AuthUser>
-    {
-       string Login(string userName,string password);
-       bool SetIsStop(int userId,bool isStop);
-    }
-}
