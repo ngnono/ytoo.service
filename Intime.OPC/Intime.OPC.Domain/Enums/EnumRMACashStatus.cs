33606965209b19acdb87090d25292fe482091cc1
hanyuxing5588 <707297069@qq.com>
2014-03-25 20:28:26 +0800

    包含在项目中

---
+++ Intime.OPC/Intime.OPC.Domain/Enums/EnumRMACashStatus.cs
@@ -0,0 +1,39 @@
+﻿// ***********************************************************************
+// Assembly         : OPCApp.Domain
+// Author           : Liuyh
+// Created          : 03-20-2014 23:59:31
+//
+// Last Modified By : Liuyh
+// Last Modified On : 03-21-2014 00:01:23
+// ***********************************************************************
+// <copyright file="EnumRMACashStatus.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System.ComponentModel;
+
+namespace Intime.OPC.Domain.Enums
+{
+    /// <summary>
+    ///     退货收银状态
+    /// </summary>
+    public enum EnumRMACashStatus
+    {
+        /// <summary>
+        ///     The no cash
+        /// </summary>
+        [Description("未送收银")] NoCash = 0,
+
+        /// <summary>
+        ///     The cash over
+        /// </summary>
+        [Description("完成收银")] CashOver = 5,
+
+        /// <summary>
+        ///     The send cash
+        /// </summary>
+        [Description("已送收银")] SendCash = 10,
+    }
+}
