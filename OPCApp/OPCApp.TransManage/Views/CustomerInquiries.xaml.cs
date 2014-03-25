ee8a7e686c3f98e80c6fe72f496ce6b1fc91ac02
shimerlj <lj841205>
2014-03-25 20:17:12 +0800

    客服

---
+++ OPCApp/OPCApp.TransManage/Views/CustomerInquiries.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MahApps.Metro;
+using OPCApp.TransManage.ViewModels;
+
+namespace OPCApp.TransManage.Views
+{
+    /// <summary>
+    /// PrintInvoiceViewModel.xaml 的交互逻辑
+    /// </summary>
+    /// 
+    [Export("StoreOut",typeof(UserControl))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class CustomerInquiries 
+    {
+        [Import("CustomerInquiriesViewModel")]
+        public object ViewModel
+        {
+            set
+            {
+                this.DataContext = value;
+            }
+            get
+            {
+                return this.DataContext;
+            }
+        }
+        public CustomerInquiries()
+        {
+            InitializeComponent();
+        }
+    }
+}
