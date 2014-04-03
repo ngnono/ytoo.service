399e7a53bba3c99563c5929fca2296e3e4984400
wangxh <huayumeng20@gmail.com>
2014-04-03 13:53:57 +0800

    set page and pagesize's default value for IMSGiftcardListRequest

--- Yintai.Hangzhou.Contract/DTO/Request/IMSGiftcardListRequest.cs
+++ Yintai.Hangzhou.Contract/DTO/Request/IMSGiftcardListRequest.cs
@@ -7,5 +7,29 @@ namespace Yintai.Hangzhou.Contract.DTO.Request
 {
     public class IMSGiftcardListRequest:PagerInfoRequest
     {
+        private int page = 1;
+        private int pageSize = 20;
+        public override int Page
+        {
+            get
+            {
+                return page;
+            }
+            set
+            {
+                page = value;
+            }
+        }
+
+        public override int Pagesize {
+            get { return pageSize; }
+            set
+            {
+                if (value > 40 || value <= 0)
+                {
+                    pageSize = 20;
+                }
+            } 
+        }
     }
 }
