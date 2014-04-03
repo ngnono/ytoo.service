3c953c5c3211d2ba2945329d552935cd46fb5998
zxy98 <zxy98@126.com>
2014-04-03 21:09:20 +0800

    zxy

--- OPCApp/OPCApp.TransManage/ViewModels/CustomerInquiriesViewModel.cs
+++ OPCApp/OPCApp.TransManage/ViewModels/CustomerInquiriesViewModel.cs
@@ -202,9 +202,9 @@ namespace OPCApp.TransManage.ViewModels
         {
             string shippingfilter =
                 string.Format(
-                    "OrderNo={0}&ExpressNo={1}&StartGoodsOutDate={2}&EndGoodsOutDate={3}&OutGoodsCode={4}&SectionId={5}&ShippingStatus={6}&CustomerPhone={7}&BrandId={8}&pageIndex={9}&pageSize={10}",
+                    "OrderNo={0}&ExpressNo={1}&StartGoodsOutDate={2}&EndGoodsOutDate={3}&OutGoodsCode={4}&StoreId={5}&ShippingStatus={6}&CustomerPhone={7}&BrandId={8}&pageIndex={9}&pageSize={10}",
                     ShippingGet.OrderNo, ShippingGet.ExpressNo, ShippingGet.StartGoodsOutDate.ToShortDateString(),
-                    ShippingGet.EndGoodsOutDate.ToShortDateString(), ShippingGet.OutGoodsCode, string.IsNullOrEmpty(ShippingGet.SectionId) ? "-1" : ShippingGet.SectionId,
+                    ShippingGet.EndGoodsOutDate.ToShortDateString(), ShippingGet.OutGoodsCode, string.IsNullOrEmpty(ShippingGet.StoreId) ? "-1" : ShippingGet.StoreId,
                     string.IsNullOrEmpty(ShippingGet.ShippingStatus) ? "-1" : ShippingGet.ShippingStatus, ShippingGet.CustomerPhone, string.IsNullOrEmpty(ShippingGet.BrandId) ? "-1" : ShippingGet.BrandId, 1, 100);
             try
             {
