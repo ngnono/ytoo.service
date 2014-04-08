258754581ba5131a7b444c5f948aa7910333d0de
liuyh208 <liuyh208@sohu.com>
2014-04-08 23:33:46 +0800

    add salermacomment

---
+++ Intime.OPC/Intime.OPC.Domain/Dto/SaleRmaDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Intime.OPC.Domain.Dto
+{
+    public class SaleRmaDto
+    {
+        public int Id { get; set; }
+        public string SaleOrderNo { get; set; }
+        public string PaymentMethodName { get; set; }
+        public double MustPayTotal { get; set; }
+        public Nullable<decimal> RealRMASumMoney { get; set; }
+        public string OrderNo { get; set; }
+        public string TransMemo { get; set; }
+        public DateTime BuyDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerRemark { get; set; }
+        public bool IfReceipt { get; set; }
+        public string ReceiptHead { get; set; }
+        public string ReceiptContent { get; set; }
+        public Nullable<decimal> StoreFee { get; set; }
+        public Nullable<decimal> CustomFee { get; set; }
+    }
+}
