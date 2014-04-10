edf94b75a3fccac4f1274aba056e750127ef4b43
hanyuxing5588 <707297069@qq.com>
2014-04-10 21:10:42 +0800

    1

---
+++ OPCApp/OPCApp.ReturnGoodsManage/Views/ReturnGoodsEntryPrintView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OPCApp.ReturnGoodsManage.View
+{
+    /// <summary>
+    ///     ReturnGoodsEntryPrintView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ReturnGoodsEntryPrintView : UserControl
+    {
+        public ReturnGoodsEntryPrintView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OPCApp/OPCApp.ReturnGoodsManage/Views/ShoppingGuideReturnGoodsReceiving.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OPCApp.ReturnGoodsManage.View
+{
+    /// <summary>
+    ///     ShoppingGuideReturnGoodsReceiving.xaml 的交互逻辑
+    /// </summary>
+    public partial class ShoppingGuideReturnGoodsReceiving : UserControl
+    {
+        public ShoppingGuideReturnGoodsReceiving()
+        {
+            InitializeComponent();
+        }
+    }
+}
