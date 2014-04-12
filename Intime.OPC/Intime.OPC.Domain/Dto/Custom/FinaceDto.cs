c564885e423a895145b8c497f64876b2f56938c8
liuyh208 <liuyh208@sohu.com>
2014-04-12 13:14:49 +0800

    add 退货赔偿审核

---
+++ Intime.OPC/Intime.OPC.Domain/Dto/Custom/FinaceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Intime.OPC.Domain.Dto.Custom
+{
+    /// <summary>
+    ///     退货赔偿审核 查询
+    /// </summary>
+    public class FinaceDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string OrderNo { get; set; }
+
+        public string SaleOrderNo { get; set; }
+    }
+}
