dfda9a2b8299bdd7053fafdc4d0da4aaaf65287b
zxy98 <zxy98@126.com>
2014-04-12 12:02:50 +0800

    zxy modi order

--- OPC.sync.job/src/Intime.OPC.Job/Order/Models/Head.cs
+++ OPC.sync.job/src/Intime.OPC.Job/Order/Models/Head.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Intime.OPC.Job.RMASplit.Models
+namespace Intime.OPC.Job.Order.Models
 {
     public class Head
     {
--- OPC.sync.job/src/Intime.OPC.Job/Order/Models/OrderStatus.cs
+++ OPC.sync.job/src/Intime.OPC.Job/Order/Models/OrderStatus.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Intime.OPC.Job.RMASplit.Models
+namespace Intime.OPC.Job.Order.Models
 {
     public class OrderStatus
     {
