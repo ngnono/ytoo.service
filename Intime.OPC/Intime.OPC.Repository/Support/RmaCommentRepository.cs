c075ac29b5cbe0c61a62605acd4afa048cfe1de1
liuyh208 <liuyh208@sohu.com>
2014-04-12 11:39:44 +0800

    add RmaCommentRepository

---
+++ Intime.OPC/Intime.OPC.Repository/Support/RmaCommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intime.OPC.Domain.Models;
+using Intime.OPC.Repository.Base;
+
+namespace Intime.OPC.Repository.Support
+{
+    public  class RmaCommentRepository : BaseRepository<OPC_RMAComment>, IRmaCommentRepository
+    {
+        public IList<OPC_RMAComment> GetByRmaID(string rmaNo)
+        {
+            return Select(t => t.RMANo == rmaNo);
+        }
+    }
+}
