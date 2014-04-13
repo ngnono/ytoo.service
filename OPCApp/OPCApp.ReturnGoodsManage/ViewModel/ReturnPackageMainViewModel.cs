863ccfccf847d64157ac2447b6eecda802031661
zxy98 <zxy98@126.com>
2014-04-13 12:57:14 +0800

    1

--- OPCApp/OPCApp.ReturnGoodsManage/ViewModel/ReturnPackageMainViewModel.cs
+++ OPCApp/OPCApp.ReturnGoodsManage/ViewModel/ReturnPackageMainViewModel.cs
@@ -5,13 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OPCApp.Domain.Customer;
+using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.Mvvm;
+using OPCApp.ReturnGoodsManage.ViewModels;
 namespace OPCApp.ReturnGoodsManage.ViewModel
 {
    [Export("ReturnPackageMainViewModel", typeof(ReturnPackageMainViewModel))]
-    public  class ReturnPackageMainViewModel
+    public  class ReturnPackageMainViewModel:BindableBase
    {
-       [Import] public ReturnPackageManageViewModel ReturnPackageManageViewModel;
-
+       private ReturnPackageManageViewModel _returnPackageManageViewModel;
+       [Import]
+       public ReturnPackageManageViewModel ReturnPackageManageViewModel
+       {
+           get { return _returnPackageManageViewModel; }
+           set { SetProperty(ref _returnPackageManageViewModel, value); }
+       }
+       private ReturnPackageVerifyViewViewModel _customerPackageVerifyViewModel;
+       [Import]
+       public ReturnPackageVerifyViewViewModel ReturnPackageVerifyViewViewModel
+       {
+           get { return _customerPackageVerifyViewModel; }
+           set { SetProperty(ref _customerPackageVerifyViewModel, value); }
+       }
        public ReturnPackageMainViewModel()
        {
                
