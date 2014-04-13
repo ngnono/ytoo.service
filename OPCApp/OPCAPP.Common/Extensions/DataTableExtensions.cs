4caff0458e9e029b05a4a82ed55c13a86817f847
zxy98 <zxy98@126.com>
2014-04-13 16:33:40 +0800

    zxy

---
+++ OPCApp/OPCAPP.Common/Extensions/DataTableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCApp.Common.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static DataTable ToDataTable<TResult>(this IEnumerable<TResult> value) where TResult : class
+        {
+            //创建属性的集合
+            List<PropertyInfo> pList = new List<PropertyInfo>();
+            Type type = typeof(TResult);
+            DataTable dt = new DataTable();
+            Array.ForEach<PropertyInfo>(type.GetProperties(), p => { pList.Add(p); dt.Columns.Add(p.Name, p.PropertyType); });
+            try
+            {
+                if (value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        //创建一个DataRow实例
+                        DataRow row = dt.NewRow();
+                        pList.ForEach(p => row[p.Name] = p.GetValue(item, null));
+                        dt.Rows.Add(row);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            return dt;
+        }
+    }
+}
