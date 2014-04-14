ee7ae145f00d1177ea1e4ebb1d8b6cd0f0e0286c
liuyh208 <liuyh208@sohu.com>
2014-04-14 21:54:09 +0800

    add sectionID 2 rma

---
+++ Intime.OPC/Intime.OPC.Domain/Dto/Custom/ShoppingGuideRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Intime.OPC.Domain.Base;
+
+namespace Intime.OPC.Domain.Dto.Custom
+{
+    /// <summary>
+    /// 导购退货收货查询
+    /// </summary>
+    public class ShoppingGuideRequest : BaseRequest
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string OrderNo { get; set; }
+    }
+}
