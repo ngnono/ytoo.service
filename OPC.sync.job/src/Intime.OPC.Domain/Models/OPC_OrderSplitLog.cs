f23003a9877b643513df227cb574180d0ec62a18
zxy98 <zxy98@126.com>
2014-04-20 01:41:35 +0800

    zxy

---
+++ OPC.sync.job/src/Intime.OPC.Domain/Models/OPC_OrderSplitLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Domain.Models
+{
+    public partial class OPC_OrderSplitLog
+    {
+        public int Id { get; set; }
+
+        public string OrderNo { get; set; }
+        public int Status { get; set; }
+        public string Reason { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
