1bdedcde954d1b38a2d595cc4e5f48cab61dc705
zxy98 <zxy98@126.com>
2014-04-20 01:40:52 +0800

    zxy

---
+++ OPC.sync.job/src/Intime.OPC.Domain/Models/Mapping/OPC_OrderSplitLogMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Domain.Models.Mapping
+{
+    public class OPC_OrderSplitLogMapper : EntityTypeConfiguration<OPC_OrderSplitLog>
+    {
+        public OPC_OrderSplitLogMapper()
+        {
+            // Primary Key
+            this.HasKey(t => t.Id);
+
+            // Properties
+            this.Property(t => t.OrderNo)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            this.Property(t => t.Reason)
+                .HasMaxLength(200);
+
+            // Table & Column Mappings
+            this.ToTable("OPC_OrderSplitLog");
+            this.Property(t => t.Id).HasColumnName("Id");
+            this.Property(t => t.OrderNo).HasColumnName("OrderNo");
+            this.Property(t => t.Reason).HasColumnName("Reason");
+            this.Property(t => t.Status).HasColumnName("Status");
+            this.Property(t => t.CreateDate).HasColumnName("CreateDate");
+
+        }
+
+    }
+}
---
+++ OPC.sync.job/src/Intime.OPC.Job/Order/OrderStatusSync/OrderStatusSyncJob.cs
@@ -0,0 +1,117 @@
+﻿using Common.Logging;
+using Intime.OPC.Domain.Enums;
+using Intime.OPC.Domain.Models;
+using Intime.OPC.Job.Order.Repository;
+using Intime.OPC.Job.Product.ProductSync;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Job.Order.OrderStatusSync
+{
+
+    [DisallowConcurrentExecution]
+    public class OrderStatusSyncJob : IJob
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        private DateTime benchTime = DateTime.Now.AddMinutes(-3600);
+        private readonly IOrderRemoteRepository _remoteRepository = new OrderRemoteRepository();
+
+        private void DoQuery(Action<IQueryable<Intime.OPC.Domain.Models.OPC_Sale>> callback)
+        {
+            using (var context = new YintaiHZhouContext())
+            {
+                var minx = context.OPC_Sale.Where(t => t.UpdatedDate > benchTime && t.Status > 0 && t.Status < (int)EnumSaleOrderStatus.SaleCompletion);
+                if (callback != null)
+                    callback(minx);
+            }
+        }
+
+        #region IJob 成员
+
+        public void Execute(IJobExecutionContext context)
+        {
+            var totalCount = 0;
+#if !DEBUG
+            JobDataMap data = context.JobDetail.JobDataMap;
+            var isRebuild = data.ContainsKey("isRebuild") ? data.GetBoolean("isRebuild") : false;
+            var interval = data.ContainsKey("intervalOfSecs") ? data.GetInt("intervalOfSecs") : 5 * 60;
+             _benchTime = DateTime.Now.AddMinutes(-interval);
+
+            if (!isRebuild)
+                benchTime = data.GetDateTime("benchtime");
+#endif
+            DoQuery(skus =>
+            {
+                totalCount = skus.Count();
+            });
+            int cursor = 0;
+            int size = 20;
+            while (cursor < totalCount)
+            {
+                List<Intime.OPC.Domain.Models.OPC_Sale> oneTimeList = null;
+                DoQuery(r => oneTimeList = r.OrderBy(t => t.OrderNo).Skip(cursor).Take(size).ToList());
+                foreach (var opc_sale in oneTimeList)
+                {
+                    Process(opc_sale);          // 同步状态到单品系统
+                }
+                cursor += size;
+            }
+        }
+        private void Process(Domain.Models.OPC_Sale opc_Sale)
+        {
+            var dataResult = _remoteRepository.GetOrderStatusById(opc_Sale);
+            if (dataResult == null)
+            {
+                Log.InfoFormat("订单信息查询失败,orderNo:{0},status:{1}", opc_Sale.SaleOrderNo, opc_Sale.Status);
+                return;
+            }
+            //订单状态为
+            int status = (int)EnumSaleOrderStatus.NotifyProduct;
+            int cashStatus = (int)EnumCashStatus.NoCash;
+            switch (dataResult.status)
+            {
+                case "1":        //导购确认提货
+                    status=(int)EnumSaleOrderStatus.ShoppingGuidePickUp;
+                    break;
+                case  "2":      // 完成收银的
+                    cashStatus = (int)EnumCashStatus.CashOver;
+                    break;
+                default:
+                    status = (int)EnumSaleOrderStatus.NotifyProduct;
+                    break;
+            }
+
+            //判断是否需要更新状态
+            if (status == (int)EnumSaleOrderStatus.NotifyProduct && cashStatus == (int)EnumCashStatus.NoCash)
+            {
+                Log.InfoFormat("单品系统状态没有更新,orderNo:{0},status:{1}", opc_Sale.SaleOrderNo, opc_Sale.Status);
+                return;
+            }
+                
+            using (var db = new YintaiHZhouContext())
+            {
+                var saleOrder = db.OPC_Sale.FirstOrDefault(a => a.SaleOrderNo == opc_Sale.SaleOrderNo);
+
+                if (status >(int)EnumSaleOrderStatus.NotifyProduct)
+                    saleOrder.Status = status;
+
+                if (cashStatus == (int)EnumCashStatus.CashOver)
+                {
+                    saleOrder.CashStatus = (int)EnumCashStatus.CashOver;
+                    saleOrder.CashDate = DateTime.Now;
+                    saleOrder.CashNum = dataResult.posseqno;
+                }
+                saleOrder.UpdatedDate = DateTime.Now;
+                saleOrder.UpdatedUser = -100;
+                db.SaveChanges();
+            }
+            Log.InfoFormat("完成订单状态更新,orderNo:{0},status:{1}", opc_Sale.OrderNo, opc_Sale.Status );
+
+        }
+        #endregion
+    }
+}
