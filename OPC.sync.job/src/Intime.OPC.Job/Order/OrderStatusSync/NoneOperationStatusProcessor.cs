8d0f683ac7319bef350a7db5f6a7b7430144ee0c
zxy98 <zxy98@126.com>
2014-04-20 17:39:17 +0800

    zxy

---
+++ OPC.sync.job/src/Intime.OPC.Job/Order/OrderStatusSync/NoneOperationStatusProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Intime.OPC.Job.Order.OrderStatusSync
+{
+    public class NoneOperationStatusProcessor:AbstractSaleOrderStatusProcessor
+    {
+        private Domain.Enums.EnumSaleOrderStatus enumSaleOrderStatus;
+
+        public NoneOperationStatusProcessor(Domain.Enums.EnumSaleOrderStatus enumSaleOrderStatus) : base(enumSaleOrderStatus) { }
+
+        public override void Process(string saleOrderNo, Models.OrderStatusResultDto statusResult)
+        {
+            
+        }
+    }
+}
