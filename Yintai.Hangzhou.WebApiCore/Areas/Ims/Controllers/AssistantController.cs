97e35836c1e74a532659eab2f9764ffa0594692a
gyyshuai@gmail.com <gyyshuai@gmail.com>
2014-04-27 14:23:29 +0800

    fix brands bug

--- Yintai.Hangzhou.WebApiCore/Areas/Ims/Controllers/AssistantController.cs
+++ Yintai.Hangzhou.WebApiCore/Areas/Ims/Controllers/AssistantController.cs
@@ -212,12 +212,12 @@ private IEFRepository<IMS_AssociateIncomeEntity> _incomeRepo;
         public ActionResult Brands(int authuid)
         {
             var linq = Context.Set<IMS_AssociateEntity>().Where(ia => ia.UserId == authuid)
-                            .GroupJoin(Context.Set<IMS_AssociateBrandEntity>().Join(Context.Set<BrandEntity>().Where(b => b.Status == (int)DataStatus.Normal),
+                            .GroupJoin(Context.Set<IMS_SectionBrandEntity>().Join(Context.Set<BrandEntity>().Where(b => b.Status == (int)DataStatus.Normal),
                                                                     o => o.BrandId,
                                                                     i => i.Id,
                                                                     (o, i) => new { AB = o, B = i }),
-                                        o => o.Id,
-                                        i => i.AB.AssociateId,
+                                        o => o.StoreId,
+                                        i => i.AB.SectionId,
                                         (o, i) => i).FirstOrDefault();
             List<dynamic> brands = null;
             if (linq != null)
