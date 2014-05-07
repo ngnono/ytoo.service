d9a725c2fedb378c8569362a348806cc1bf881c0
zxy98 <zxy98@126.com>
2014-05-07 16:45:17 +0800

    zxy

---
+++ OPC.sync.job/src/Intime.OPC.Domain/Models/OPC_RMANotificationLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Domain.Models
+{
+    public class OPC_RMANotificationLog
+    {
+        public int Id { get; set; }
+        public string RMANo { get; set; }
+        public int Status { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public int CreateUser { get; set; }
+        public string Message { get; set; }
+    }
+}
