10cc4ed719d8b9504bbf70adc9e90a99c2f2775f
zxy98 <zxy98@126.com>
2014-05-08 17:21:43 +0800

    zxy add RMASync

---
+++ OPC.sync.job/src/Intime.OPC.Job/RMASync/AbstractRMASaleStatusProcessor.cs
@@ -0,0 +1,21 @@
+﻿using Intime.OPC.Domain.Enums;
+using Intime.OPC.Job.Order.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intime.OPC.Job.RMASync
+{
+    public abstract class AbstractRMASaleStatusProcessor
+    {
+        protected EnumRMAStatus _status;
+        protected AbstractRMASaleStatusProcessor(EnumRMAStatus status)
+        {
+            this._status = status;
+        }
+        public abstract void Process(string RMANo, OrderStatusResultDto statusResult);
+
+    }
+}
