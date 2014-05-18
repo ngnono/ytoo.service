d033ee47b8c35fe8eee1420859754aaa915075ea
what.what you say <ngnono@gmail.com>
2014-05-18 15:27:00 +0800

    add not mapped attrib 4 entity

--- Intime.OPC/Intime.OPC.Domain/Partials/Models/OPC_Sale.cs
+++ Intime.OPC/Intime.OPC.Domain/Partials/Models/OPC_Sale.cs
@@ -1,37 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Intime.OPC.Domain.Models
 {
-    public partial class SaleOrderModel 
+    [NotMapped]
+    public partial class SaleOrderModel : OPC_Sale
     {
-        public int Id { get; set; }
-        public string OrderNo { get; set; }
-        public string SaleOrderNo { get; set; }
-        public int SalesType { get; set; }
-        public Nullable<int> ShipViaId { get; set; }
-        public int Status { get; set; }
-        public string ShippingCode { get; set; }
-        public Nullable<int> ShippingSaleId { get; set; }
-        public Nullable<decimal> ShippingFee { get; set; }
-        public Nullable<int> ShippingStatus { get; set; }
-        public string ShippingRemark { get; set; }
-        public System.DateTime SellDate { get; set; }
-        public Nullable<bool> IfTrans { get; set; }
-        public Nullable<int> TransStatus { get; set; }
-        public decimal SalesAmount { get; set; }
-        public int SalesCount { get; set; }
-        public Nullable<int> CashStatus { get; set; }
-        public string CashNum { get; set; }
-        public Nullable<System.DateTime> CashDate { get; set; }
-        public Nullable<int> SectionId { get; set; }
-        public Nullable<int> PrintTimes { get; set; }
-        public string Remark { get; set; }
-        public Nullable<System.DateTime> RemarkDate { get; set; }
-        public System.DateTime CreatedDate { get; set; }
-        public int CreatedUser { get; set; }
-        public Nullable<System.DateTime> UpdatedDate { get; set; }
-        public Nullable<int> UpdatedUser { get; set; }
-
         public virtual Store Store { get; set; }
 
         public virtual Section Section { get; set; }
