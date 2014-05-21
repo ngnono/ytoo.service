7ed96b122a4adb64a15983423fd859cce5e296b3
zxy98 <zxy98@126.com>
2014-05-21 18:20:13 +0800

    zxy

--- OPCApp/OPCAPP.Domain/Dto/Customer/RMADto.cs
+++ OPCApp/OPCAPP.Domain/Dto/Customer/RMADto.cs
@@ -7,61 +7,55 @@ namespace OPCApp.Domain.Customer
         public bool IsSelected { get; set; }
         public int Id { get; set; }
         public string SaleOrderNo { get; set; }
-
         /// <summary>
-        ///     退货单号
+        /// 退货单号
         /// </summary>
         /// <value>The rma no.</value>
         public string RMANo { get; set; }
-
         public string SourceDesc { get; set; }
-
         /// <summary>
-        ///     退货总数
+        /// 退货总数
         /// </summary>
         /// <value>The count.</value>
         public int? Count { get; set; }
 
+        /// <summary>
+        /// 赔偿金额
+        /// </summary>
+        /// <value>The refund amount.</value>
         public decimal RefundAmount { get; set; }
 
         public string OrderNo { get; set; }
-
         /// <summary>
-        ///     退货类型
+        /// 退货类型
         /// </summary>
         /// <value>The type of the rma.</value>
         public int RMAType { get; set; }
-
         /// <summary>
-        ///     退货单状态ID
+        /// 退货单状态ID
         /// </summary>
         /// <value>The status.</value>
         public int Status { get; set; }
-
         /// <summary>
-        ///     退货单状态
+        /// 退货单状态
         /// </summary>
         /// <value>The name of the status.</value>
         public string StatusName { get; set; }
-
         /// <summary>
-        ///     退货总金额
+        /// 退货总金额
         /// </summary>
         /// <value>The refund amount.</value>
         public decimal RMAAmount { get; set; }
 
         public int? UserId { get; set; }
-
         /// <summary>
-        ///     退货原因
+        /// 退货原因
         /// </summary>
         /// <value>The rma reason.</value>
         public string RMAReason { get; set; }
-
         public string ContactPerson { get; set; }
-
         /// <summary>
-        ///     退货时间
+        /// 要求退货时间
         /// </summary>
         /// <value>The created date.</value>
         public DateTime CreatedDate { get; set; }
@@ -70,57 +64,106 @@ namespace OPCApp.Domain.Customer
         public string StoreName { get; set; }
 
         /// <summary>
-        ///     退货收银状态
+        /// 退货收银状态
         /// </summary>
         /// <value>The name of the rma cash status.</value>
         public string RmaCashStatusName { get; set; }
 
         /// <summary>
-        ///     退货状态
+        /// 退货状态
         /// </summary>
         /// <value>The name of the rma cash status.</value>
         public string RmaStatusName { get; set; }
 
         /// <summary>
-        ///     专柜码
+        /// 专柜码
         /// </summary>
         /// <value>The name of the rma cash status.</value>
-        public string 专柜码 { get; set; }
+        public string SectionCode { get; set; }
 
         /// <summary>
-        ///     收银流水号
+        /// 收银流水号
         /// </summary>
         /// <value>The cash number.</value>
         public string CashNum { get; set; }
 
         /// <summary>
-        ///     收银时间
+        /// 收银时间
         /// </summary>
         /// <value>The cash number.</value>
         public DateTime? CashDate { get; set; }
 
         /// <summary>
-        ///     退货收银流水号
+        /// 退货收银流水号
         /// </summary>
         /// <value>The cash number.</value>
         public string RmaCashNum { get; set; }
 
         /// <summary>
-        ///     退货收银时间
+        /// 退货收银时间
         /// </summary>
         /// <value>The cash number.</value>
         public DateTime? RmaCashDate { get; set; }
 
         /// <summary>
-        ///     退货时间
+        /// 退货时间
         /// </summary>
-        /// <value>The back date.<alue>
-        public DateTime BackDate { get; set; }
+        /// <value>The back date.</value>
+        public System.DateTime BackDate { get; set; }
 
         /// <summary>
-        ///     支付方式
+        /// 支付方式
         /// </summary>
-        /// <value>The cash number.<alue>
+        /// <value>The cash number.</value>
         public string PayType { get; set; }
+
+        #region 原OPC_SaleRMA
+        public string PaymentMethodName { get; set; }
+        /// <summary>
+        /// 订单来源
+        /// </summary>
+        public string OrderSource { get; set; }
+        /// <summary>
+        /// 订单运费
+        /// </summary>
+        public decimal? OrderTransFee { get; set; }
+        public double MustPayTotal { get; set; }
+        public Nullable<decimal> RealRMASumMoney { get; set; }
+        public string TransMemo { get; set; }
+        public DateTime BuyDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerRemark { get; set; }
+        public bool IfReceipt { get; set; }
+        public string ReceiptHead { get; set; }
+        public string ReceiptContent { get; set; }
+        /// <summary>
+        /// 公司应付
+        /// </summary>
+        /// <value>The store fee.</value>
+        public decimal? StoreFee { get; set; }
+        /// <summary>
+        /// 客户应付
+        /// </summary>
+        /// <value>The custom fee.</value>
+        public decimal? CustomFee { get; set; }
+
+        /// <summary>
+        /// 客服同意时间
+        /// </summary>
+        /// <value>The service agree date.</value>
+        public DateTime? ServiceAgreeDate { get; set; }
+
+        /// <summary>
+        /// 应退总金额[待定]
+        /// </summary>
+        public decimal? RecoverableSumMoney { get; set; }
+        /// <summary>
+        /// 赔偿 暂定[应退总金额]
+        /// </summary>
+        public decimal? CompensationFee { get; set; }
+
+        #endregion
     }
 }
