e94d60807439bc9187335e0e646e0c63b63f6833
wangxh <huayumeng20@gmail.com>
2014-05-24 23:28:57 +0800

    no modification

--- OPC.sync.job/src/Intime.OPC.Job/Product/ProductSync/Supports/Intime/Jobs/ProductPropertySyncJob.cs
+++ OPC.sync.job/src/Intime.OPC.Job/Product/ProductSync/Supports/Intime/Jobs/ProductPropertySyncJob.cs
@@ -6,6 +6,7 @@ using Intime.OPC.Job.Product.ProductSync.Supports.Intime.Repository;
 using Quartz;
 using System;
 using System.Threading;
+using PropertyValue = Intime.O2O.ApiClient.Domain.PropertyValue;
 
 namespace Intime.OPC.Job.Product.ProductSync.Supports.Intime.Jobs
 {
@@ -37,77 +38,89 @@ namespace Intime.OPC.Job.Product.ProductSync.Supports.Intime.Jobs
             while (true)
             {
                 var properties =
-                    _remoteRepository.GetProductProperties(pageIndex++, PageSize, lastUpdateDateTime).ToList();
+                    _remoteRepository.GetProductProperties(pageIndex++, PageSize, lastUpdateDateTime);
 
-                if (properties.Count == 0)
+                if (properties == null || !properties.Any())
                 {
                     Log.ErrorFormat("没有可同步的信息,pageIndex:{0},pageSize:{1},lastUpdateDatetime:{2}", pageIndex, PageSize, lastUpdateDateTime);
                     break;
                 }
                 foreach (var p in properties)
                 {
-                    using (var db = new YintaiHZhouContext())
+                    try
                     {
-                        var product = db.Products.FirstOrDefault(i => i.CreatedDate > benchDate &&
-                                                                      i.SkuCode.ToUpper() == p.ProductCode.ToUpper());
-                        if (product == null)
-                        {
-                            Log.ErrorFormat("属性对应商品未同步，商品款号:({0})", p.ProductCode);
-                            continue;
-                        }
-                        var property =
-                            db.ProductProperties.FirstOrDefault(
-                                x => x.ProductId == product.Id && x.ChannelPropertyId == p.PropertyId);
-                        if (property == null)
-                        {
-                            property = db.ProductProperties.Add(new ProductProperty()
-                            {
-                                ProductId = product.Id,
-                                ChannelPropertyId = p.PropertyId,
-                                IsColor = false,
-                                IsSize = false,
-                                PropertyDesc = p.PropertyName,
-                                SortOrder = 0,
-                                Status = 1,
-                                UpdateDate = DateTime.Now,
-                                UpdateUser = SystemDefine.SystemUser
-                            });
-                        }
-                        else
-                        {
-                            property.PropertyDesc = p.PropertyName;
-                            property.UpdateDate = DateTime.Now;
-                        }
-                        db.SaveChanges();
-
-                        var channelValueId = Convert.ToInt32(p.PropertyValueId);
-
-                        var pValue =
-                            db.ProductPropertyValues.FirstOrDefault(
-                                x =>
-                                    x.PropertyId == property.Id &&
-                                    x.ChannelValueId == channelValueId);
-                        if (pValue == null)
-                        {
-                            db.ProductPropertyValues.Add(new ProductPropertyValue()
-                            {
-                                ChannelValueId = channelValueId,
-                                CreateDate = DateTime.Now,
-                                PropertyId = property.Id,
-                                Status = 1,
-                                UpdateDate = DateTime.Now,
-                                ValueDesc = p.ValueName,
-                            });
-                        }
-                        else
-                        {
-                            pValue.ValueDesc = p.ValueName;
-                            pValue.UpdateDate = DateTime.Now;
-                        }
-                        db.SaveChanges();
+                        this.Process(p, benchDate);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.ErrorFormat(ex.Message);
                     }
                 }
                 Thread.Sleep(500);
+            }
+        }
+
+        private void Process(PropertyValue p, DateTime benchDate)
+        {
+            using (var db = new YintaiHZhouContext())
+            {
+                var product = db.Products.FirstOrDefault(i => i.CreatedDate > benchDate &&
+                                                              i.SkuCode.ToUpper() == p.ProductCode.ToUpper());
+                if (product == null)
+                {
+                    Log.ErrorFormat("属性对应商品未同步，商品款号:({0})", p.ProductCode);
+                    return;
+                }
+                var property =
+                    db.ProductProperties.FirstOrDefault(
+                        x => x.ProductId == product.Id && x.ChannelPropertyId == p.PropertyId);
+                if (property == null)
+                {
+                    property = db.ProductProperties.Add(new ProductProperty()
+                    {
+                        ProductId = product.Id,
+                        ChannelPropertyId = p.PropertyId,
+                        IsColor = false,
+                        IsSize = false,
+                        PropertyDesc = p.PropertyName,
+                        SortOrder = 0,
+                        Status = 1,
+                        UpdateDate = DateTime.Now,
+                        UpdateUser = SystemDefine.SystemUser
+                    });
+                }
+                else
+                {
+                    property.PropertyDesc = p.PropertyName;
+                    property.UpdateDate = DateTime.Now;
+                }
+                db.SaveChanges();
+
+                var channelValueId = Convert.ToInt32(p.PropertyValueId);
+
+                var pValue =
+                    db.ProductPropertyValues.FirstOrDefault(
+                        x =>
+                            x.PropertyId == property.Id &&
+                            x.ChannelValueId == channelValueId);
+                if (pValue == null)
+                {
+                    db.ProductPropertyValues.Add(new ProductPropertyValue()
+                    {
+                        ChannelValueId = channelValueId,
+                        CreateDate = DateTime.Now,
+                        PropertyId = property.Id,
+                        Status = 1,
+                        UpdateDate = DateTime.Now,
+                        ValueDesc = p.ValueName,
+                    });
+                }
+                else
+                {
+                    pValue.ValueDesc = p.ValueName;
+                    pValue.UpdateDate = DateTime.Now;
+                }
+                db.SaveChanges();
             }
         }
     }
